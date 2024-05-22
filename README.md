db3b37f9f3c2abb4b5ac2ba51a85a2b9c43742d4
Shubham harne <155050237+LethalDestroyer@users.noreply.github.com>
2024-05-23 00:31:17 +0530

    initial commit- staging it .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # first-step-fastapi
 A first step of fast api learning
+
+Step to run the fast api program.
---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from typing import List, Optional
+
+app = FastAPI()
+
+db = [
+    {"id":1, "size" : "s", "fuel":"petrol", "doors":4},
+    {"id":2, "size" : "m", "fuel":"Deisel", "doors":3},
+    {"id":3, "size" : "l", "fuel":"petrol", "doors":2},
+    {"id":4, "size" : "s", "fuel":"electrix", "doors":2},
+    {"id":5, "size" : "l", "fuel":"petrol", "doors":4},
+]
+
+
+@app.get("/")
+def get_cars(size: Optional[str]= None, fuel: Optional[str]= None):
+    filter_db = db
+
+    if size:
+        filter_db = [car for car in filter_db if car["size"] == size]
+    if fuel:
+        filter_db = [car for car in filter_db if car["fuel"] == fuel]
+    return filter_db
+
